3875daed3537d3d8566420d6c6aef58176244291
Shivani Budhbhatti <shivanibu@cybage.com>
2018-07-05 12:59:08 +0530

    Added New mathod to New Test

--- Toy_Demo/src/test/java/example/NewTest.java
+++ Toy_Demo/src/test/java/example/NewTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+
 public class NewTest {
   
 	private WebDriver driver;
@@ -15,9 +16,20 @@ public class NewTest {
 		  driver = new ChromeDriver();
 		  driver.manage().window().maximize();
 		  driver.get("http://www.toyota.com/contact-a-dealer/");
-		  
+		  org.testng.Assert.fail("you wandered onto the wrong path");
 	
 	}
 	 
-
+	@Test
+	public void h() {
+		
+		 System.setProperty("webdriver.chrome.driver", "D:/FreeTime/Jar Files/chromedriver_win32/chromedriver.exe");
+		  driver = new ChromeDriver();
+		  driver.manage().window().maximize();
+		  driver.get("http://www.toyota.com/");
+		  
+		  System.out.println("Inside H Method");
+		  
+	
+	}
 }
